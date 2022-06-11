30b4acb48957701f96b2fb781e90997f13077195
baonguyenbxl <69773267+baonguyenbxl@users.noreply.github.com>
2022-06-11 23:43:02 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,6 +4,3 @@ import { callShell } from './eos';
 const connection = new Endpoint();
 
 const io = getSocket( connection.getServer() );
-
-//console.log( callShell( "ps ux" ) );
-//callShell("ps ux").then((value)=>{console.log(value)}).catch((err)=>{console.log(err)})
